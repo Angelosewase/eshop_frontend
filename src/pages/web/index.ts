15a7715f8917839e0acba2e949b0e0dec248833c
Polo <148433691+MugishaProsper@users.noreply.github.com>
2025-03-21 17:24:24 +0200

    resolved errors minor

--- src/pages/web/index.ts
+++ src/pages/web/index.ts
@@ -6,7 +6,7 @@ export { default as Orders } from "./Orders";
 export { default as ProductDetails } from "./ProductDetails";
 export { default as Products } from "./Products";
 export { default as Explore } from "./Explore";
-export { default as Checkout } from "./Checkout.tsx";
+export { default as Checkout } from "./Checkout";
 export { default as Profile } from "./Profile";
 export { default as OrderConfirmation } from "./OrderConfirmation";
 export { default as PaymentMethods } from "./PaymentMethods";
